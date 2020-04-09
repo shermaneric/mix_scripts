21e1d313e2dc8b8e68a9918b20dffc35c2e690e5
Eric Sherman <eric.sherman@tausight.com>
2020-04-09 09:00:43 -0700

    jenkinsfile test

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,42 @@
+properties( [
+        buildDiscarder( logRotator( numToKeepStr: '15', artifactNumToKeepStr: '3') ),
+        disableConcurrentBuilds(),
+])
+
+timestamps {
+    timeout(time: 30, unit: 'MINUTES') {
+        //environment variable needs to match the Tausight github credential in http://<Jenkins instance>/credentials/
+        GIT_CREDENTIALS_ID = 'tausight-github-key-shermaneric'
+        SLACK_CHANNEL='#jenkins'
+
+        node('eric_test_node') {
+            try {
+                stage("Prep and Checkout SCM") {
+                    deleteDir() // deleteDir ensures fresh checkouts of repos each time
+                    checkout scm
+                }
+                stage("Update something") {
+                    sh "echo \$(date) > myfile"
+                }
+                currentBuild.result = 'SUCCESS'
+            } catch (e) {
+                currentBuild.result = "FAILURE"
+                println e
+                error()
+            } finally {
+                sendSlackNotification()
+            }
+        }
+    }
+}
+
+def tagAndPush() {
+    sshagent (credentials: [GIT_CREDENTIALS_ID]) {
+        sh('git config user.email "shermaneric@gmail.com"')
+        sh('git config user.name "Eric SHerman"')
+        sh('git push')
+    }
+}
+
+
+
